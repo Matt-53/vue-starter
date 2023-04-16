75e66776003807690a40f8e0b42a775a99339418
Mateusz Krawczyk <mateuszkr@student.agh.edu.pl>
2023-04-16 11:25:04 +0200

    Updated website

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,52 @@
 <template>
   <div class="my-app">
-    <h1>Hello MWO!</h1>
+    <h1>WItaj w sysytemie do zapisów na zajęcia!</h1>
+
+    <div v-if="loggedIn">
+      Zalogowany!
+      <div>
+        <button @click="backToMainPage()">Wyloguj</button>
+      </div>
+    </div>
+    <div v-else>
+      <input type="email" v-model="email" placeholder="Type your e-mail">
+      <button @click="checkIfProperAddress()">Zaloguj</button>
+    </div>
+
+    <div v-if="!email.includes('@')"> E-mail powinine zawierać "@"!</div>
+
+<!--    <div v-else-if="email.length < 10">Ale masz krótki adres ({{email.length}})!</div>-->
+<!--    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>-->
+<!--    <div v-else>Twój adres e-mail jest stanowczo za długi ({{email.length}})!</div>-->
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      loggedIn: false
+    };
+  },
 
+  methods: {
+    alertMyEmail() {
+      alert(this.email);
+    },
+
+    checkIfProperAddress() {
+      if (this.email.includes('@')) {
+        this.loggedIn = true;
+      }
+    },
+
+    backToMainPage() {
+      this.loggedIn = false;
+      this.email = '';
+    }
+  }
 }
 </script>
 
